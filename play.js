6bee53c4b5b7c477bc7d8108d0e3ec81ee8e50bb
Darren Lin <darrenlin815@hotmail.com>
2022-10-26 18:19:22 -0700

    Moved handleUserInput and setupInput into the input.js file and exported both functions using ES6 object shorthand

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  handleUserInput();
-  return stdin;
-};
-
-const handleUserInput = function() {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      console.log('Sssssee you later!')
-      process.exit();
-    }
-  })
-};
 
 setupInput();
