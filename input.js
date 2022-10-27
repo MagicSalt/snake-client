2010fee27d2ef7e4fd6fe2fd76059c7f5d5cfada
Darren Lin <darrenlin815@hotmail.com>
2022-10-26 18:19:44 -0700

    Contains handleUserInput and setupInput functions

---
+++ input.js
@@ -0,0 +1,19 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+};
+
+const handleUserInput = function() {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      console.log('Sssssee you later!')
+      process.exit();
+    }
+  })
+};
+
+module.exports = { setupInput };
