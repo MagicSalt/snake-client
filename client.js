70ac2f2e4d39f7c299e1a4b0855358db6121cda3
Darren Lin <darrenlin815@hotmail.com>
2022-10-26 17:47:25 -0700

    exported connect function into client

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  conn.on('data', (data) => {
+    console.log('got the data but', data);
+  });
+  
+  conn.setEncoding('utf8');
+
+  return conn;
+};
+
+module.exports = { connect };
